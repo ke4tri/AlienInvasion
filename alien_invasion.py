0fd0a001ac0abed420f391fad662c2b0e6eea755
Chen <jen@localhost.localdomain>
2024-01-01 17:34:53 -0600

    adding helpers and classes

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage gae assets and behavior"""
@@ -16,26 +17,33 @@ class AlienInvasion:
        # self.screen = pygame.display.set_mode((1200,800))
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
         #set background color
-        self.bg_color = (230,230,230)
+        #self.bg_color = (230,230,230)
+       
     
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            #Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            #Redraw the screen during each pass through the loop.
-            #self.screen.fill(self.bg_color)
-            self.screen.fill(self.settings.bg_color)
-
-            #Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+        
 
+    def _check_events(self):
+        #Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+                
+    def _update_screen(self):
+        #Redraw the screen during each pass through the loop.
+        #self.screen.fill(self.bg_color)
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        #Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #Make a game instance, and run the game.
